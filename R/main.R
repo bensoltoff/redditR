bc3b87da9f822d6d7d5cabe2699604148be4a729
Benjamin Soltoff <ben@bensoltoff.com>
2016-08-16 09:51:12 -0500

    get_page <- fetch .json content
    get_elements <- extract individual elements from get_page

--- R/main.R
+++ R/main.R
@@ -6,13 +6,35 @@
 #' @return A character string containing the json data from the Reddit API call
 #'
 #' @examples
-#' get_page_content("http://www.reddit.com/")
-#' get_page_content("http://www.reddit.com/r/politics/")
-#' get_page_content("https://www.reddit.com/r/funny/comments/4xuzyl/")
-#' get_page_content("https://www.reddit.com/r/funny/comments/4xuzyl/usain_bolt_celebrating_his_olympic_victory_with/")
-get_page_content <- function(link){
+#' get_page("http://www.reddit.com/")
+#' get_page("http://www.reddit.com/r/politics/")
+#' get_page("https://www.reddit.com/r/funny/comments/4xuzyl/")
+#' get_page("https://www.reddit.com/r/funny/comments/4xuzyl/usain_bolt_celebrating_his_olympic_victory_with/")
+get_page <- function(link, user_agent = NA){
   # check to see if link is appended with .json
-  if(endsWith(link, ".json")) url <- paste0(link, ".json")
+  if(!endsWith(link, ".json")) link <- paste0(link, ".json")
 
-  return(readLines(link))
+  page <- RCurl::getURL(link, httpheader = c('User-Agent' = user_agent))
+
+  return(page)
+}
+
+#' Parse page into individual elements
+#'
+#' @param page Object created by `get_page`
+#'
+#' @return
+#'
+#' @importFrom magrittr "%>%"
+#'
+get_elements <- function(page){
+  page %>%
+    tidyjson::as.tbl_json(.) %>%
+    tidyjson::gather_array(.) %>%
+    tidyjson::gather_keys(.) %>%
+    dplyr::filter(key == "data") %>%
+    tidyjson::gather_keys(.) %>%
+    dplyr::filter(key == "children") %>%
+    tidyjson::gather_array(.) %>%
+    tidyjson::spread_values(type = tidyjson::jstring("kind"))
 }
